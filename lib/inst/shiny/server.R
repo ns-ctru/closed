15910d9c82e2446158c88e380215bf5e38e72978
Neil Shephard <n.shephard@sheffield.ac.uk>
2016-03-08 09:53:16 +0000

    Initial upload to GitHub

---
+++ lib/inst/shiny/server.R
@@ -0,0 +1,10 @@
+# Load libraries
+library(dplyr)
+library(ggplot2)
+library(hubble)
+library(magrittr)
+library(shiny)
+library(RColorBrewer)
+# Define Server Output
+shinyServer(function(input, output){
+})
