b38c641ebf65a700c8c6326484ff00f3007739f2
Neil Shephard <n.shephard@sheffield.ac.uk>
2016-04-14 09:41:51 +0000

    Have data to work with, restructured filtering of data based on options

--- lib/R/closed_regress.R
+++ lib/R/closed_regress.R
@@ -10,66 +10,101 @@
 #' the EDs functioning
 #'
 #' @param df Data frame to analyse.
-#' @param sites List of sites to include, should be lower-case and list the case and control sites of interest.
-#' @param indicator Performance indicator to assess.
+#' @param group The case site you wish to analyse, choose from \code{Bishop Auckland General Hospital} (default) | \code{Hemel Hempstead Hospital} | \code{Newark Hospital} | \code{Rochdale Infirmary} | \code{University Hospital of Hartlepool} | \code{All}.
+#' @param controls The controls which should be included and analysed, choose from \code{matched} (default and only option if \code{group = 'All'}) | \code{pooled}.
+#' @param measure The performance indicator to assess.
+#' @param sub.measure The sub-measure performance indicator to assess.
 #' @param steps List of steps (dummy variables) to include in time-series analysis.
-#' @param fit.with Which package to fit Prais-Winsten regression with, options are
-#'                 \code{panelAR} | \code{prais} | \code{both} (default)
+#' @param fit.with Which package to fit Prais-Winsten regression with, options are  \code{both} (default) | \code{panelAR} | \code{prais}
 #' @param plot Generate time-series plot
 #' @param theme GGplot2 theme to use (only relevant if \code{plot = TRUE})
 #' @param dose Perform dose response model based on median travel time to ED for LSOA.
 #' @param latex Produce results table in LaTeX format using Stargazer.
 #' @param html Produce results table in HTML format using Stargazer.
 #'
-#' @return A list of results depending on the options specified
+#' @return A list of results depending on the options specified.
 #'
 #' @examples
+#'
+#' ## Run mortality analyses for Bishop Auckland and its matched site
+#' ## producing time-series plot, step and dose models using both the
+#' ## panelAR and prais package.
+#'
+#' ts.mortality <- closed_regress(df       = ,)
+#'
 #'
 #' @references
 #'
 #' @export
-closed_regress<- function(df        = test,
-                          sites     = c('Bishop Auckland', 'Hemel Hempstead'),
-                          indicator = 'mortality',
-                          steps     = c(closure),
-                          fit.with  = 'both',
-                          plot      = TRUE,
-                          theme     = NULL,
-                          dose      = FALSE,
-                          latex     = FALSE,
-                          html      = FALSE,
+closed_regress<- function(df          = test,
+                          group       = c('Bishop Auckland General Hospital'),
+                          controls    = 'matched',
+                          measure     = 'ed attendance',
+                          sub.measure = 'all',
+                          steps       = c('closure'),
+                          fit.with    = 'both',
+                          plot        = TRUE,
+                          theme       = NULL,
+                          dose        = FALSE,
+                          latex       = FALSE,
+                          html        = FALSE,
                           ...){
     ## Initialise results list for returning everything
     results <- list()
     ## Build the regerssion model
-    .formula <- formulate()
-    ## Filter the data based on the specified indicator
+    ## ToDo - Create formula from specified steps
+    ## .formula <- reformulate(repsonse   = 'value',
+    ##                         termalbels = c('town', 'time.to.ed', steps))
+    ## Convert to data frame
+    df <- as.data.frame(df)
+    ## Convert variable names for ease of typing within this function
+    ## (ESS artefact, hitting underscore inserts '<-' so lots of underscores are
+    ## tedious to type)
+    names(df) <- names(df) %>%
+                 gsub("_", ".", x = .)
+    ## Filter the data
     ## ToDo - Modify to reflect the options should exclude all sub-measures
-    if(indicator == 'mortality.rate'){
-        df <- dplyr::filter(df, measure == indicator)
+    if(!is.na(measure) & !is.na(sub.measure)){
+        df <- dplyr::filter(df, measure == indicator & sub_measure)
     }
     else{
-        df <- dplyr::filter(df, sub.measure == indicator)
+        stop('You must specify an indicator and the sub measure you wish to analyse.\n\n')
     }
     ## Filter based on sites to include
-    df <- dplyr::filter(df, site.short %in% sites )
+    if(!is.na(group)){
+        df <- dplyr::filter(df, group == group)
+    }
+    else{
+        stop('You must specify the group you wish to analyse.\n\n')
+    }
+    ## Select intervention site and the desired controls
+    if(controls != 'matched control' | controls != 'pooled control'){
+        stop('You must specify the controls you wish to analyse.\n\n')
+    }
+    else{
+        t <- c('intervention', controls)
+    }
+    df <- dplyr::filter(df, site_type %in% t)
+    rm(t)
+    results$df <- df
     ## Plot the data
     if(plot == TRUE){
         ## Conditionally set the graph title
         ## ToDo - Complete
-        if(indicator      == 'mortality')  title <- 'Mortality'
-        else if(indicator == 'admissions') title <- 'Admissions'
-        else if(indicator == 'other')      title <- 'Other'
-        title.site <- site[1]
+        if(indicator      == 'ed attendance')  indicator.title <- 'Mode of ED Attendance'
+        else if(indicator == 'mortality')      indicator.title <- 'Mortality'
+        else if(indicator == 'other')          indicator.title <- 'Other'
+        if(controls == 'matched control')      control.title   <- 'Matched Control'
+        else if(controls == 'pooled control')  control.title   <- 'Pooled Control'
+        ## Generate graph
         results$ts.plot.events <- ggplot(data    = df,
                                          mapping = aes(yearmonth,
                                                        value)) +
-            geom_line() +
-            stat_seas(start = c(2000, 1), frequency = 2) +
-            ggtitle(paste0(title,
-                           " at ",
-                           title.site,
-                           " and "))
+                                  geom_line() +
+                                  stat_seas(start = c(2000, 1), frequency = 2) +
+                                  ggtitle(paste0(indicator.title, " at ",
+                                                 group, " (",
+                                                 control.title, ")"))
         ## Apply the user-specified theme
         if(!is.null(theme)){
 
