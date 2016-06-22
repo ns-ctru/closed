06d278bbdee449dff5662567ae3d0d642bc85b4d
Neil Shephard <n.shephard@sheffield.ac.uk>
2016-06-22 15:57:55 +0100

    internal function balance added (not yet used though)

--- lib/R/closed_models.R
+++ lib/R/closed_models.R
@@ -294,62 +294,64 @@ closed_models <- function(df.lsoa         = ed_attendances_by_mode_measure,
     }
     #######################################################################
     ## Identify and remove spurious data points                          ##
+    ## ToDo : Make this an internal function                             ##
     #######################################################################
-    ## if(tidy == TRUE){
-    ##     ## Condition on the indicator and sub indicator
-    ##     if(indicator == 'ed attendances' & sub.indicator == 'any'){
-    ##         if('Bishop Auckland' %in% sites){
-    ##             df <- mutate(df,
-    ##                          value = ifelse(relative.month %in% c(1, 6), yes = NA, no = value))
-    ##         }
-    ##     }
-    ##     else if(indicator == 'ed attendances' & sub.indicator == 'ambulance'){
-    ##         if('Bishop Auckland' %in% sites){
-    ##             df <- mutate(df,
-    ##                          value = ifelse(relative.month %in% c(1, 6), yes = NA, no = value))
-    ##         }
-    ##     }
-    ##     else if(indicator == 'ed attendances' & sub.indicator == 'other'){
-    ##         if('Bishop Auckland' %in% sites){
-    ##             df <- mutate(df,
-    ##                          value = ifelse(relative.month %in% c(1, 6), yes = NA, no = value))
-    ##         }
-    ##     }
-    ##     else if(indicator == 'unnecessary ed attendances' & sub.indicator == 'unnecessary ed attendances'){
-    ##         if('Bishop Auckland' %in% sites){
-    ##             df <- mutate(df,
-    ##                          value = ifelse(relative.month %in% c(1, 6), yes = NA, no = value))
-    ##         }
-    ##     }
-    ##     else if(indicator == 'ed attendances admitted' & sub.indicator == 'all'){
-    ##         if('Bishop Auckland' %in% sites){
-    ##             df <- mutate(df,
-    ##                          value = ifelse(relative.month %in% c(1, 6), yes = NA, no = value))
-    ##         }
-    ##     }
-    ##     else if(indicator == 'ed attendances admitted' & sub.indicator == 'admitted'){
-    ##         if('Bishop Auckland' %in% sites){
-    ##             df <- mutate(df,
-    ##                          value = ifelse(relative.month %in% c(1, 6), yes = NA, no = value))
-    ##         }
-    ##     }
-    ##     else if(indicator == 'ed attendances admitted' & sub.indicator == 'fraction'){
-    ##         if('Hemel Hempstead' %in% sites){
-    ##             df <- mutate(df,
-    ##                          value = ifelse(relative.month %in% c(1), yes = NA, no = value))
-    ##         }
-    ##     }
-    ##     else if(indicator == 'critical care stays' & sub.indicator == 'all'){
-    ##         if('Newark' %in% sites){
-    ##             df <- mutate(df,
-    ##                          value = ifelse(relative.month %in% c(47), yes = NA, no = value))
-    ##         }
-    ##     }
-    ##     ## Remove data points that are now missing
-    ##     if(join == TRUE){
-    ##         df <- filter(df, !is.na(value))
-    ##     }
-    ## }
+    balance <- function(x,
+                        sites = c('Bishop Auckland')){
+        ## Condition on the indicator and sub indicator
+        if(indicator == 'ed attendances' & sub.indicator == 'any'){
+            if('Bishop Auckland' %in% sites){
+                df <- mutate(df,
+                             value = ifelse(relative.month %in% c(1, 6), yes = NA, no = value))
+            }
+        }
+        else if(indicator == 'ed attendances' & sub.indicator == 'ambulance'){
+            if('Bishop Auckland' %in% sites){
+                df <- mutate(df,
+                             value = ifelse(relative.month %in% c(1, 6), yes = NA, no = value))
+            }
+        }
+        else if(indicator == 'ed attendances' & sub.indicator == 'other'){
+            if('Bishop Auckland' %in% sites){
+                df <- mutate(df,
+                             value = ifelse(relative.month %in% c(1, 6), yes = NA, no = value))
+            }
+        }
+        else if(indicator == 'unnecessary ed attendances' & sub.indicator == 'unnecessary ed attendances'){
+            if('Bishop Auckland' %in% sites){
+                df <- mutate(df,
+                             value = ifelse(relative.month %in% c(1, 6), yes = NA, no = value))
+            }
+        }
+        else if(indicator == 'ed attendances admitted' & sub.indicator == 'all'){
+            if('Bishop Auckland' %in% sites){
+                df <- mutate(df,
+                             value = ifelse(relative.month %in% c(1, 6), yes = NA, no = value))
+            }
+        }
+        else if(indicator == 'ed attendances admitted' & sub.indicator == 'admitted'){
+            if('Bishop Auckland' %in% sites){
+                df <- mutate(df,
+                             value = ifelse(relative.month %in% c(1, 6), yes = NA, no = value))
+            }
+        }
+        else if(indicator == 'ed attendances admitted' & sub.indicator == 'fraction'){
+            if('Hemel Hempstead' %in% sites){
+                df <- mutate(df,
+                             value = ifelse(relative.month %in% c(1), yes = NA, no = value))
+            }
+        }
+        else if(indicator == 'critical care stays' & sub.indicator == 'all'){
+            if('Newark' %in% sites){
+                df <- mutate(df,
+                             value = ifelse(relative.month %in% c(47), yes = NA, no = value))
+            }
+        }
+        ## Remove data points that are now missing
+        if(join == TRUE){
+            df <- filter(df, !is.na(value))
+        }
+    }
     #######################################################################
     ## Internal functions (to save typing)                               ##
     #######################################################################
