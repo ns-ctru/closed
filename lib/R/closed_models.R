1e3c82c608ae2b3372946c6451ac2779e3e10ab9
Neil Shephard <n.shephard@sheffield.ac.uk>
2016-08-10 13:06:22 +0100

    Refinement of summary statistics table header

--- lib/R/closed_models.R
+++ lib/R/closed_models.R
@@ -170,39 +170,75 @@ closed_models <- function(df.lsoa         = ed_attendances_by_mode_measure,
                                    mean     = mean(value, na.rm = TRUE),
                                    sd       = sd(value, na.rm = TRUE),
                                    min      = min(value, na.rm = TRUE),
-                                   max      = min(value, na.rm = TRUE),
+                                   max      = max(value, na.rm = TRUE),
                                    p25      = quantile(value, probs = 0.25, na.rm = TRUE),
                                    p50      = quantile(value, probs = 0.50, na.rm = TRUE),
                                    p75      = quantile(value, probs = 0.75, na.rm = TRUE))
     results$summary.table.head <- results$summary.df
-    results$summary.table.head <- mutate(results$summary.df,
+    results$summary.table.head <- mutate(results$summary.table.head,
                                          mean.sd    = paste0(formatC(mean, digits = digits, format = 'f'),
                                                              ' (',
                                                              formatC(sd, digits = digits, format = 'f'),
                                                              ')'))
-    results$summary.table.head <- mutate(results$summary.df,
-                                         median.iqr = paste0(formatC(p50, digits = digits, format = 'f'),
+    results$summary.table.head <- mutate(results$summary.table.head,
+                                         median.iqr = paste0(formatC(p50, digits = 1, format = 'f'),
                                                              ' (',
-                                                             formatC(p25, digits = digits, format = 'f'),
+                                                             formatC(p25, digits = 1, format = 'f'),
                                                              '-',
-                                                             formatC(p75, digits = digits, format = 'f'),
+                                                             formatC(p75, digits = 1, format = 'f'),
                                                              ')'))
-    results$summary.table.head <- mutate(results$summary.df,
-                                         min.max    = paste0(formatC(min, digits = digits, format = 'f'),
+    results$summary.table.head <- mutate(results$summary.table.head,
+                                         min.max    = paste0(formatC(min, digits = 0, format = 'f'),
                                                              '-',
-                                                             formatC(max, digits = digits, format = 'f')))
+                                                             formatC(max, digits = 0, format = 'f')))
     results$summary.table.head <- dplyr::select(results$summary.table.head,
-                                                town, before.after, n, mean.sd, median.iqr, min.max)
+                                                town, before.after, mean.sd, median.iqr, min.max)
     ## Reshape the table header
     results$summary.table.head <- melt(results$summary.table.head, id.vars = c('town', 'before.after')) %>%
                                   dcast(town ~ before.after + variable)
+    ## Order the data
+    results$summary.table.head$order <- 0
+    results$summary.table.head$order[results$summary.table.head$town == 'Bishop Auckland'] <- 1
+    results$summary.table.head$order[results$summary.table.head$town == 'Whitehaven']      <- 2
+    results$summary.table.head$order[results$summary.table.head$town == 'Salford']         <- 3
+    results$summary.table.head$order[results$summary.table.head$town == 'Scarborough']     <- 4
+    results$summary.table.head$order[results$summary.table.head$town == 'Hartlepool']      <- 5
+    results$summary.table.head$order[results$summary.table.head$town == 'Grimsby']         <- 6
+    results$summary.table.head$order[results$summary.table.head$town == 'Blackburn']       <- 7
+    results$summary.table.head$order[results$summary.table.head$town == 'Wigan']           <- 8
+    results$summary.table.head$order[results$summary.table.head$town == 'Hemel Hempstead'] <- 9
+    results$summary.table.head$order[results$summary.table.head$town == 'Warwick']         <- 10
+    results$summary.table.head$order[results$summary.table.head$town == 'Basingstoke']     <- 11
+    results$summary.table.head$order[results$summary.table.head$town == 'Yeovil']          <- 12
+    results$summary.table.head$order[results$summary.table.head$town == 'Newark']          <- 13
+    results$summary.table.head$order[results$summary.table.head$town == 'Southport']       <- 14
+    results$summary.table.head$order[results$summary.table.head$town == 'Carlisle']        <- 15
+    results$summary.table.head$order[results$summary.table.head$town == 'Salisbury']       <- 16
+    results$summary.table.head$order[results$summary.table.head$town == 'Rochdale']        <- 17
+    results$summary.table.head$order[results$summary.table.head$town == 'Rotherham']       <- 18
+    results$summary.table.head$order[results$summary.table.head$town == 'Scunthorpe']      <- 19
+    results$summary.table.head$order[results$summary.table.head$town == 'Wansbeck']        <- 20
+    results$summary.table.head <- arrange(results$summary.table.head, order)
+    results$summary.table.head <- dplyr::select(results$summary.table.head,
+                                                town,
+                                                Before_mean.sd, Before_median.iqr, Before_min.max,
+                                                After_mean.sd, After_median.iqr, After_min.max)
+    ## Add in grouping to facilitate subsetting later
+    results$summary.table.head$group <- NA
+    results$summary.table.head$group[results$summary.table.head$town %in% c('Bishop Auckland', 'Whitehaven', 'Salfrod', 'Scarborough')] <- 'Bishop Auckland'
+    results$summary.table.head$group[results$summary.table.head$town %in% c('Hartlepool', 'Grimsby', 'Blackburn', 'Wigan')] <- 'Hartlepool'
+    results$summary.table.head$group[results$summary.table.head$town %in% c('Hemel Hempstead', 'Warwick', 'Basingstoke', 'Yeovil')] <- 'Hemel Hempstead'
+    results$summary.table.head$group[results$summary.table.head$town %in% c('Newark', 'Southport', 'Carlisle', 'Salisbury')] <- 'Newark'
+    results$summary.table.head$group[results$summary.table.head$town %in% c('Rochdale', 'Rotherham', 'Scunthorpe', 'Wansbeck')] <- 'Rochdale'
+    ## Add indicator for primary control
+    results$summary.table.head$town[results$summary.table.head$town %in% c('Whitehaven', 'Grimsby', 'Warwick', 'Southport', 'Rotherham')] <- paste0(results$summary.table.head$town[results$summary.table.head$town %in% c('Whitehaven', 'Grimsby', 'Warwick', 'Southport', 'Rotherham')], ' (Primary)')
     #######################################################################
     ## Derive a seasonal indicator                                       ##
     #######################################################################
-    ## ## print("Debug 5")
-    ## ## print("LSOA")
+    ## print("Debug 5")
+    ## print("LSOA")
     ## dim(df.lsoa) %>% print()
-    ## ## print("TRUST")
+    ## print("TRUST")
     ## dim(df.trust) %>% print()
     df.lsoa$season <- 1
     df.lsoa <- within(df.lsoa,{
@@ -420,23 +456,18 @@ closed_models <- function(df.lsoa         = ed_attendances_by_mode_measure,
         }
         if(!is.null(bishop.coef)){
             column.names <- c(column.names, 'Bishop Auckland')
-
         }
         if(!is.null(hartlepool.coef)){
             column.names <- c(column.names, 'Hartlepool')
-
         }
         if(!is.null(hemel.coef)){
             column.names <- c(column.names, 'Hemel Hempstead')
-
         }
         if(!is.null(newark.coef)){
             column.names <- c(column.names, 'Newark')
-
         }
         if(!is.null(rochdale.coef)){
             column.names <- c(column.names, 'Rochdale')
-
         }
         names(coef$coef) <- column.names
         ## Derive a caption for the table
